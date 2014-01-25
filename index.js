a0c37c293386a1ccc3f3f871fd509a64237c3482
Jacob Groundwater <jacob@newrelic.com>
2014-01-25 12:24:31 -0800

    add .keys() method to list keys

--- index.js
+++ index.js
@@ -15,4 +15,18 @@ Config.prototype.get = function get(key) {
   return this.head[key];
 };
 
+Config.prototype.keys = function keys() {
+  var keys = [];
+
+  var head = this.head;
+  while (head) {
+    Object.keys(head).forEach(function (key) {
+      if (keys.indexOf(key) < 0) keys.push(key);
+    });
+    head = head.__proto__;
+  }
+
+  return keys;
+}
+
 module.exports = Config;
--- test.js
+++ test.js
@@ -10,6 +10,21 @@ config.load({
   'three': 3
 });
 
+var keys = config.keys();
+
+// do not contain duplicates
+assert.equal(keys.length, 3);
+
+assert(keys.indexOf('one')   > -1);
+assert(keys.indexOf('two')   > -1);
+assert(keys.indexOf('three') > -1);
+
+// confirmation bias check
+assert(keys.indexOf('four') == -1);
+
 assert.equal(config.get('one')   , 1);
 assert.equal(config.get('two')   , 'two');
 assert.equal(config.get('three') , 3);
+
+// confirmation bias check
+assert(!config.get('four'));
